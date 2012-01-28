637e246fdbd4ff0ff049e4aacef131788218b72b
Andrey Anosov <a.anosov@parcsis.org>
2012-01-28 18:32:34 +0400

    Пример java-приложения для создания консольного headless-браузера с поддержкой javascript (на основе библиотеки HtmlUnit)

---
+++ src/htmlunit.java
@@ -0,0 +1,34 @@
+import java.util.logging.Level;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class htmlunit {
+	public static void main(String[] args) throws Exception {
+		// Отключаем отображение ошибок
+		java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+
+		String url = "";				// для тестовых нужд
+		if (url.length() == 0 && args.length > 0) {
+			url = args[0];				// читаем url из параметров в консоли
+		}
+
+		// Если есть URL, то запрашиваем снимок страницы (вместе с контентом, генерируемым ajax)
+		if (url.length() > 0) {
+			// Используем движок Firefox 3.6 (он совместим с большинством библиотек, например jQuery)
+			WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
+			// Нет необходимости обрабатывать CSS
+			webClient.setCssEnabled(false);
+			// Ждем, пока отработают ajax-запросы, выполняемые при загрузке страницы
+			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+			// Запрашиваем и рендерим веб-страницу
+			HtmlPage page = webClient.getPage(url);
+			// Выводим исходный код страницы в консоль
+			System.out.println(page.asXml());
+			// Закрываем headless-браузер, освобождаем память
+			webClient.closeAllWindows();
+		}
+	}
+}
