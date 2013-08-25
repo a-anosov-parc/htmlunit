6951ba49084bcc78eaf1c72f1533dc65c18e1a46
a-anosov-parc <a.anosov@parcsis.org>
2013-08-25 16:42:38 +0400

    Более универсальное решение проблемы с незакрытыми пустыми строчными тегами

--- src/htmlunit.java
+++ src/htmlunit.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.regex.*;
 import java.util.logging.Level;
 
@@ -20,33 +21,62 @@ public class htmlunit {
 		if (url.length() > 0) {
 			// Используем движок Firefox
 			WebClient webClient = new WebClient(BrowserVersion.FIREFOX_17);
+			
 			// Нет необходимости обрабатывать CSS
 			webClient.getOptions().setCssEnabled(false);
+			
 			// Ждем, пока отработают ajax-запросы, выполняемые при загрузке страницы
 			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+			
 			// Запрашиваем и рендерим веб-страницу
 			HtmlPage page = webClient.getPage(url);
+			
 			// Даем время выполниться setTimeout в javascript
 			webClient.waitForBackgroundJavaScript(10000);
+			
 			// Запоминаем doctype
 			Pattern doctypePattern = Pattern.compile("<!DOCTYPE((.|\n|\r)*?)>");
 			String sourceHtml = page.getWebResponse().getContentAsString();
 			Matcher doctypes = doctypePattern.matcher(sourceHtml);
 			doctypes.find();
 			String doctype = doctypes.group(0);
-			// Заменяем doctype xml на исходный
+			
+			// Получаем xml запрашиваемой страницы
 			String resultXml = page.asXml();
+			
+			// Заменяем doctype xml на исходный
 			resultXml = resultXml.replaceAll("<\\?((.|\n|\r)*?)\\?>", doctype);
+			
 			// Убираем все теги script
 			resultXml = resultXml.replaceAll("<script\\b[^<]*(?:(?!<\\/script>)<[^<]*)*<\\/script>", "");
-			// Исправляем кривую замену тегов <i></i> на <i/>
-			Pattern inlineTagsPattern = Pattern.compile("(<i\\s.*)\\/>");
+			
+			// Исправляем кривую замену строчных тегов в xml
+			List<String> allMatches = new ArrayList<String>();
+			List<String> allMatchesTagNames = new ArrayList<String>();
+			List<String> inlineClosedTagsList = Arrays.asList("b", "big", "i", "small", "tt", "abbr", "acronym", 
+					"cite", "code", "dfn", "em", "kbd", "strong", "samp", "var", "a", "bdo", "map", "object", 
+					"q", "script", "span", "sub", "sup", "button", "label", "select", "textarea");
+			Pattern inlineTagsPattern = Pattern.compile("<([a-z]+)\\s.*\\/>");
 			Matcher inlineTags = inlineTagsPattern.matcher(resultXml);
-			if (inlineTags.find()) {
-				resultXml = inlineTags.replaceAll("$1></i>");
+			
+			while (inlineTags.find()) {
+				if (inlineClosedTagsList.contains(inlineTags.group(1))) {
+					allMatches.add(inlineTags.group());
+					allMatchesTagNames.add(inlineTags.group(1));
+				}
 			}
+			
+			for (int i = 0; i < allMatches.size(); i++) {
+				String replacementTag = allMatches.get(i);
+				int replacementTagLength = replacementTag.length();
+				String fixedTag = new StringBuilder(replacementTag.substring(0, replacementTagLength - 2))
+								  .append("></").append(allMatchesTagNames.get(i)).append(">").toString();
+				resultXml = resultXml.replaceAll(replacementTag, fixedTag);
+			}
+			
 			// Выводим исходный код страницы в консоль
 			System.out.println(resultXml);
+			
 			// Закрываем headless-браузер, освобождаем память
 			webClient.closeAllWindows();
 		}
